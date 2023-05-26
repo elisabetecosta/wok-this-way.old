176143b3f1c1e8bcc37ac05b7fa0b3492d0bba96
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-05-26 19:25:57 +0100

    testing db connection

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ const reviewRoutes = require('./routes/reviews')
 
 const MongoStore = require('connect-mongo')
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/wok-this-way'
+//|| 'mongodb://localhost:27017/wok-this-way'
+
+const dbUrl = process.env.DB_URL 
 
 
 // Connects to mongo database
