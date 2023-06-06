f8ce7bd474786e73c87de75c1d34704c796183ff
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-06-06 12:16:45 +0100

    modified comments

--- public/js/validateForms.js
+++ public/js/validateForms.js
@@ -2,10 +2,10 @@
 (() => {
     'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    // Fetches all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
+    // Loops over them and prevents submission
     Array.from(forms)
         .forEach(form => {
             form.addEventListener('submit', event => {
